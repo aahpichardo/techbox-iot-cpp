0d6c427c7a565e5e4cbc690291411414b3aa0dd6
Angel Pichardo <aahpichardo@gmail.com>
2024-02-24 18:52:06 -0600

    Funcionan
    funcionan todos los reles

--- final-techbox/final-techbox.ino
+++ final-techbox/final-techbox.ino
@@ -1,9 +1,21 @@
 #include <SoftwareSerial.h> //Para el ESP32 se necesita la libreria EspSoftwareSerial by Dirk Kaar, Peter Lerup
-SoftwareSerial mySerial(16, 17); // TX (amarillo), RX (verde)
+#include <WiFi.h>
+SoftwareSerial mySerial(16, 17); // TX (amarillo), RX (verde) //Necesario para el escaner QR
 
+//Pines de los relevadores
 const byte relayOne = 25;
+const byte relayTwo = 26;
+const byte relayThree = 27;
+const byte relayFour = 33;
+
+//Conexión WIFI
+#define WIFI_SSID "IZZI-AB02"
+#define WIFI_PASSWORD "3C046117AB02"
 
 void setup() {
+  //Conexión WIFI
+  
+
   //Inicia setup del scanner QR
   Serial.begin(9600);
   mySerial.begin(9600);
@@ -12,7 +24,9 @@ void setup() {
 
   //Inicia setup de relés
   pinMode(relayOne, OUTPUT);
-
+  pinMode(relayTwo, OUTPUT);
+  pinMode(relayThree, OUTPUT);
+  pinMode(relayFour, OUTPUT);
   //Fin setup relés
 
 }
@@ -33,27 +47,45 @@ void loop() {
     }
     
     Serial.println();
-
+    
     // Verifica si el contenido del código QR no está vacío antes de activar el relé
-    if (!qrCode.isEmpty()) {
-      // Desactiva el relé
-      digitalWrite(relayOne, LOW); //LOW es para que se active
-      qrReadRecently = true;  // Establece la variable de estado en true
-      // Espera 3 segundos
-      delay(3000);
-      // Activa el relé
-      digitalWrite(relayOne, HIGH); //HIGH es para que se apague
-
+      if(!qrCode.isEmpty()){
+        if(qrCode == "Extension"){
+          digitalWrite(relayOne, LOW);
+          delay(3000);
+          digitalWrite(relayOne, HIGH);
+      }else if( qrCode == "Ethernet"){
+        digitalWrite(relayTwo, LOW);
+        delay(3000);
+        digitalWrite(relayTwo, HIGH);
+        Serial.println("Encendiendo relay dos");
+      }else if(qrCode == "Adaptador"){
+        digitalWrite(relayThree, LOW);
+        delay(3000);
+        digitalWrite(relayThree, HIGH);
+        Serial.println("Encendiendo relay tres");
+      }else if(qrCode == "HDMI"){
+        digitalWrite(relayFour, LOW);
+        delay(3000);
+        digitalWrite(relayFour, HIGH);
+        Serial.println("Encendiendo relay cuatro");
+      }
       // Limpia qrCode para la próxima lectura
       qrCode = "";
     }
   } else {
     // Si no hay datos disponibles, asegúrate de que el relé esté activado
     digitalWrite(relayOne, HIGH);
+    digitalWrite(relayTwo, HIGH);
+    digitalWrite(relayThree, HIGH);
+    digitalWrite(relayFour, HIGH);
   }
 
   // Tu código principal puede continuar aquí...
-}
+}//fin loop
+
+
+
 
 
 
